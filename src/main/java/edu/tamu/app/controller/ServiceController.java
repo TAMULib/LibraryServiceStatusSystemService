43cba5af42496db07403ca732e12c9864ab81bf3
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-06-26 16:25:47 -0500

    Added Service Controller

---
+++ src/main/java/edu/tamu/app/controller/ServiceController.java
@@ -0,0 +1,79 @@
+package edu.tamu.app.controller;
+
+import static edu.tamu.framework.enums.ApiResponseType.SUCCESS;
+import static edu.tamu.framework.enums.BusinessValidationType.CREATE;
+import static edu.tamu.framework.enums.BusinessValidationType.DELETE;
+import static edu.tamu.framework.enums.BusinessValidationType.EXISTS;
+import static edu.tamu.framework.enums.BusinessValidationType.NONEXISTS;
+import static edu.tamu.framework.enums.BusinessValidationType.UPDATE;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+import org.tdl.vireo.model.EmailTemplate;
+
+import edu.tamu.app.model.Service;
+import edu.tamu.app.model.repo.ServiceRepo;
+import edu.tamu.framework.aspect.annotation.ApiMapping;
+import edu.tamu.framework.aspect.annotation.ApiValidatedModel;
+import edu.tamu.framework.aspect.annotation.ApiValidation;
+import edu.tamu.framework.aspect.annotation.ApiVariable;
+import edu.tamu.framework.aspect.annotation.Auth;
+import edu.tamu.framework.model.ApiResponse;
+
+@RestController
+@ApiMapping("/service")
+public class ServiceController {
+    
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    private ServiceRepo serviceRepo;
+
+    @ApiMapping("/all")
+    @Auth(role="ROLE_STAFF")
+    public ApiResponse getAllServices() {
+        return new ApiResponse(SUCCESS, serviceRepo.findAll());
+    }
+    
+    @ApiMapping("/public")
+    @Auth(role="ROLE_ANONYMOUS")
+    public ApiResponse getPublicServices() {
+        return new ApiResponse(SUCCESS, serviceRepo.findByIsPublic(true));
+    }
+    
+    @ApiMapping("/get/{id}")
+    @Auth(role = "ROLE_ANONYMOUS")
+    public ApiResponse getService(@ApiVariable Long id) {
+        Service service = serviceRepo.findOne(id);
+        return new ApiResponse(SUCCESS, service);
+    }
+    
+    @ApiMapping("/create")
+    @Auth(role = "SERVICE_MANAGER")
+    @ApiValidation(business = { @ApiValidation.Business(value = CREATE), @ApiValidation.Business(value = EXISTS) })
+    public ApiResponse createService(@ApiValidatedModel Service service) {
+        service = serviceRepo.create(service.getName(), service.getStatus(), service.getIsPublic(), service.getOnShortList());
+        simpMessagingTemplate.convertAndSend("/channel/service", new ApiResponse(SUCCESS, serviceRepo.findOne(service.getId())));
+        return new ApiResponse(SUCCESS, service);
+    }
+    
+    @ApiMapping("/update")
+    @Auth(role = "SERVICE_MANAGER")
+    @ApiValidation(business = { @ApiValidation.Business(value = UPDATE), @ApiValidation.Business(value = NONEXISTS) })
+    public ApiResponse updateService(@ApiValidatedModel Service service) {
+        service = serviceRepo.save(service);
+        simpMessagingTemplate.convertAndSend("/channel/service", new ApiResponse(SUCCESS, serviceRepo.findOne(service.getId())));
+        return new ApiResponse(SUCCESS);
+    }
+    
+    @ApiMapping("/remove")
+    @Auth(role = "SERVICE_MANAGER")
+    @ApiValidation(business = { @ApiValidation.Business(value = DELETE), @ApiValidation.Business(value = NONEXISTS) })
+    public ApiResponse removeService(@ApiValidatedModel Service service) {
+        serviceRepo.delete(service);
+        simpMessagingTemplate.convertAndSend("/channel/service", new ApiResponse(SUCCESS, serviceRepo.findAll()));
+        return new ApiResponse(SUCCESS);
+    }
+}

af48ca209e6bd79b27eb58c4d944238d6d48993a
jeremythuff <jeremythuff@gmail.com>
2017-06-28 15:17:07 -0500

    Added new classes for overal status

---
+++ src/main/java/edu/tamu/app/model/OverallStatus.java
@@ -0,0 +1,31 @@
+package edu.tamu.app.model;
+
+import edu.tamu.app.enums.OverallMessageType;
+
+public class OverallStatus {
+	
+	private String message;
+	
+	private  OverallMessageType type;
+	
+	public OverallStatus() {
+		
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public OverallMessageType getType() {
+		return type;
+	}
+
+	public void setType(OverallMessageType type) {
+		this.type = type;
+	}
+
+}

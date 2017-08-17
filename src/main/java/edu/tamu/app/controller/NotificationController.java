21319f9ab345242adf02e01bb869f4bd076f0a91
William Welling <wwelling@library.tamu.edu>
2017-08-17 13:47:32 -0500

    case insensitive notifacation query param

--- src/main/java/edu/tamu/app/controller/NotificationController.java
+++ src/main/java/edu/tamu/app/controller/NotificationController.java
@@ -68,7 +68,7 @@ public class NotificationController {
     @SkipAop
     @RequestMapping("/notification/active")
     public String getActiveNotifications(@RequestParam(value = "location", defaultValue = "ALL") String location) {
-        return buildNotificationHtml(notificationRepo.activeNotificationsByLocation(location));
+        return buildNotificationHtml(notificationRepo.activeNotificationsByLocation(location.toLowerCase()));
     }
 
     private String buildNotificationHtml(List<Notification> notifications) {

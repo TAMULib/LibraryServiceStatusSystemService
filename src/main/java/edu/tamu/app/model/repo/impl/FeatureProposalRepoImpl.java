d1cdd000e48babc44ba83b69b5910a190f4ff9cb
William Welling <wwelling@library.tamu.edu>
2018-08-22 11:38:01 -0500

    ignore voters when saving feature proposal

--- src/main/java/edu/tamu/app/model/repo/impl/FeatureProposalRepoImpl.java
+++ src/main/java/edu/tamu/app/model/repo/impl/FeatureProposalRepoImpl.java
@@ -4,6 +4,7 @@ import static edu.tamu.weaver.response.ApiStatus.SUCCESS;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
@@ -65,16 +66,19 @@ public class FeatureProposalRepoImpl implements FeatureProposalRepoCustom {
     }
 
     @Override
-    public FeatureProposal update(FeatureProposal featureProposal) {
-        for (Idea idea : featureProposal.getIdeas()) {
+    public FeatureProposal update(FeatureProposal featureProposalToUpdate) {
+        FeatureProposal persistedFeatureProposal = featureProposalRepo.findOne(featureProposalToUpdate.getId());
+        // NOTE: ignore voters on update to avoid concurrency issue of save after votes occur
+        BeanUtils.copyProperties(featureProposalToUpdate, persistedFeatureProposal, "voters");
+        for (Idea idea : persistedFeatureProposal.getIdeas()) {
             idea.setState(IdeaState.ELEVATED);
-            idea.setFeatureProposal(featureProposal);
+            idea.setFeatureProposal(persistedFeatureProposal);
             idea = ideaRepo.save(idea);
             simpMessagingTemplate.convertAndSend("/channel/ideas/update", new ApiResponse(SUCCESS, idea));
         }
-        featureProposal = featureProposalRepo.save(featureProposal);
-        simpMessagingTemplate.convertAndSend("/channel/feature-proposals/update", new ApiResponse(SUCCESS, featureProposal));
-        return featureProposal;
+        persistedFeatureProposal = featureProposalRepo.save(persistedFeatureProposal);
+        simpMessagingTemplate.convertAndSend("/channel/feature-proposals/update", new ApiResponse(SUCCESS, persistedFeatureProposal));
+        return persistedFeatureProposal;
     }
 
     @Override
